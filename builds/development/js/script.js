c848ce0a08140f2c1a5c82c5c0910f435773c429
Norman Quates Jr <norman@edaxc.com>
2016-07-19 20:27:37 -0500

    started working on navbar menu

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -1,6 +1,29 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-// var jQuery = require('jquery');
-// var bscodereq = require('bootstrap');
+$ = require('jquery');
+$( "#brand" ).click(function() {
+  $( "#hamburger" ).click();
+});
+
+// $('.navbar-collapse ul li a').click(function(){ 
+//             $( "#hamburger" ).click();
+// });
+$(function () { 
+        $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function () { 
+                $('.navbar-toggle:visible').click(); 
+        }); 
+});
+$( "#edatadetails,#antaresdetails" ).click(function(e){
+	var target = this.hash;
+    var $target = $(target);
+    $('html, body').stop().animate({
+		'scrollTop': ($target.offset().top)-100
+	},1000);
+	
+	$( "#hamburger" ).click();
+	
+	return true;
+});
+
 
 var $, fill;
 
--- components/scripts/_base.js
+++ components/scripts/_base.js
@@ -1,2 +1,25 @@
-// var jQuery = require('jquery');
-// var bscodereq = require('bootstrap');
+$ = require('jquery');
+$( "#brand" ).click(function() {
+  $( "#hamburger" ).click();
+});
+
+// $('.navbar-collapse ul li a').click(function(){ 
+//             $( "#hamburger" ).click();
+// });
+$(function () { 
+        $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function () { 
+                $('.navbar-toggle:visible').click(); 
+        }); 
+});
+$( "#edatadetails,#antaresdetails" ).click(function(e){
+	var target = this.hash;
+    var $target = $(target);
+    $('html, body').stop().animate({
+		'scrollTop': ($target.offset().top)-100
+	},1000);
+	
+	$( "#hamburger" ).click();
+	
+	return true;
+});
+
